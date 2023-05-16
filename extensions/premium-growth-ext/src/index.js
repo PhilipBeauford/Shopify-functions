30e0fc216a4d9c60cd043b428808dd6a35b7c2ba
Philip Beauford <philipbeauford@github.com>
2023-05-16 01:28:42 -0700

    discount function updated w configuration

--- extensions/premium-growth-ext/src/index.js
+++ extensions/premium-growth-ext/src/index.js
@@ -1,63 +1,70 @@
 // @ts-check
 import { DiscountApplicationStrategy } from "../generated/api";
-import isEven from "is-even";
 
 /**
- * @typedef {import("../generated/api").InputQuery} InputQuery
- * @typedef {import("../generated/api").FunctionResult} FunctionResult
- * @typedef {import("../generated/api").ProductVariant} ProductVariant
- * @typedef {import("../generated/api").Target} Target
- */
+* @typedef {import("../generated/api").InputQuery} InputQuery
+* @typedef {import("../generated/api").FunctionResult} FunctionResult
+* @typedef {import("../generated/api").Target} Target
+* @typedef {import("../generated/api").ProductVariant} ProductVariant
+*/
 
 /**
- * @type {FunctionResult}
- */
+* @type {FunctionResult}
+*/
 const EMPTY_DISCOUNT = {
   discountApplicationStrategy: DiscountApplicationStrategy.First,
   discounts: [],
 };
 
 export default /**
- * @param {InputQuery} input
- * @returns {FunctionResult}
- */
-(input) => {
-  // Merchant configuration for your function can be stored on discount metafields
-  /**
-   * @type {{
-   *    percentage: number,
-   * }}
-   */
-  const configuration = JSON.parse(
-    input?.discountNode?.metafield?.value ?? "{}"
-  );
+* @param {InputQuery} input
+* @returns {FunctionResult}
+*/
+  (input) => {
+    // Define a type for your configuration, and parse it from the metafield
+    /**
+    * @type {{
+    *  quantity: number
+    *  percentage: number
+    * }}
+    */
+    const configuration = JSON.parse(
+      input?.discountNode?.metafield?.value ?? "{}"
+    );
+    if (!configuration.quantity || !configuration.percentage) {
+      return EMPTY_DISCOUNT;
+    }
 
-  /** @type {Target[]} */
-  const targets = input.cart.lines
-    // only cart lines with an even quantity
-    .filter((line) => isEven(line.quantity))
-    // you need a variant id for a product discount
-    .filter((line) => line.merchandise.__typename == "ProductVariant")
-    .map((line) => /** @type {ProductVariant} */ (line.merchandise))
-    .map((variant) => {
-      return {
-        productVariant: {
-          id: variant.id,
-        },
-      };
-    });
+    const targets = input.cart.lines
+      // Use the configured quantity instead of a hardcoded value
+      .filter(line => line.quantity >= configuration.quantity &&
+        line.merchandise.__typename == "ProductVariant")
+      .map(line => {
+        const variant = /** @type {ProductVariant} */ (line.merchandise);
+        return /** @type {Target} */ ({
+          productVariant: {
+            id: variant.id
+          }
+        });
+      });
 
-  return {
-    discountApplicationStrategy: DiscountApplicationStrategy.First,
-    discounts: [
-      {
-        targets,
-        value: {
-          percentage: {
-            value: configuration.percentage,
-          },
-        },
-      },
-    ],
+    if (!targets.length) {
+      console.error("No cart lines qualify for volume discount.");
+      return EMPTY_DISCOUNT;
+    }
+
+    return {
+      discounts: [
+        {
+          targets,
+          value: {
+            percentage: {
+              // Use the configured percentage instead of a hardcoded value
+              value: configuration.percentage.toString()
+            }
+          }
+        }
+      ],
+      discountApplicationStrategy: DiscountApplicationStrategy.First
+    };
   };
-};
